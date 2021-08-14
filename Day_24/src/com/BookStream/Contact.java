7be09744fe83d6070d59c19cbb7290e9c5e09a6a
Srikanth_M <srikanthMidathapalli@gmail.com>
2021-08-14 17:33:43 +0530

    contact commit

---
+++ Day_24/src/com/BookStream/Contact.java
@@ -0,0 +1,17 @@
+package com.BookStream;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Contact {
+
+	public static Map<String, List<String>> map=new HashMap<String, List<String>>();
+
+	public String FullName;
+	public String address;
+	public String cityName;
+	public String stateName;
+	public String zipCode;
+	public String phoneNumber;
+	public String emailAddress;
+}
